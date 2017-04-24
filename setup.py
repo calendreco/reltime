fe711c3439e82a8186f2c500fb4d102de6c920e5
brencklebox <mark.brenckle@gmail.com>
2017-04-24 14:55:19 -0400

    updated reltime to support time text and time ranges as well as dates

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
-import sys, os
+import sys
+import os
 
 version = '0.1.0'
 
@@ -7,15 +8,17 @@ setup(name='reltime',
       version=version,
       description="find and normalize time information in unstructured text",
       long_description="""\
-the reltime package uses regular expressions to identify common ways of speaking about dates and times in unstructred text,  as well as to normalize those dates and times into a standard format with the addition of a base date/time for the text.""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords= ['relative', 'time', 'normalize', 'text', 'date'],
+the reltime package uses regular expressions to identify common ways of speaking about
+dates and times in unstructred text,  as well as to normalize those dates and times into
+a standard format with the addition of a base date/time for the text.""",
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords=['relative', 'time', 'normalize', 'text', 'date'],
       author='Mark Brenckle',
       author_email='mark@liveapp.com',
       url='http://live.xyz',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      download_url = 'https://github.com/calendreco/reltime/archive/v0.1.tar.gz',
+      download_url='https://github.com/calendreco/reltime/archive/v0.1.tar.gz',
       include_package_data=True,
       zip_safe=True,
       install_requires=[
